c98ef44b4fc3607d8804bb732d1e81aa3fef2266
Darren Yap <56539537+DarrenYapZW@users.noreply.github.com>
2019-11-23 15:32:30 +0800

    Add files via upload

--- portfolio/test_login.py
+++ portfolio/test_login.py
@@ -14,7 +14,32 @@ def test_login_fail_blank():
     elemPassword.send_keys("")
 
     elemPassword.send_keys(Keys.RETURN)
-    #assert "Django" in driver.title
+    assert driver.find_element_by_css_selector("input:invalid")
+
+def test_login_username_only():
+    driver = webdriver.Chrome()
+    driver.get("http://localhost:8000/admin/login/?next=/admin/")
+    
+    elemName = driver.find_element_by_name("username")
+    elemName.send_keys("JunYoung")
+
+    elemPassword = driver.find_element_by_name("password")
+    elemPassword.send_keys("")
+
+    elemPassword.send_keys(Keys.RETURN)
+    assert driver.find_element_by_css_selector("input:invalid")
+
+def test_login_password_only():
+    driver = webdriver.Chrome()
+    driver.get("http://localhost:8000/admin/login/?next=/admin/")
+    
+    elemName = driver.find_element_by_name("username")
+    elemName.send_keys("")
+
+    elemPassword = driver.find_element_by_name("password")
+    elemPassword.send_keys("Iwanttoeat50burger")
+
+    elemPassword.send_keys(Keys.RETURN)
     assert driver.find_element_by_css_selector("input:invalid")
 
 def test_login_fail():
@@ -22,15 +47,15 @@ def test_login_fail():
     driver.get("http://localhost:8000/admin/login/?next=/admin/")
     
     elemName = driver.find_element_by_name("username")
-    elemName.send_keys("benny")
+    elemName.send_keys("Benny")
 
     elemPassword = driver.find_element_by_name("password")
     elemPassword.send_keys("Password123A")
 
     elemPassword.send_keys(Keys.RETURN)
+    assert driver.find_element_by_class_name("errornote")
 
-
-def test_login():
+def test_login_pass():
     driver = webdriver.Chrome()
     driver.get("http://localhost:8000/admin/login/?next=/admin/")
 
@@ -41,6 +66,8 @@ def test_login():
     elemPassword.send_keys("Iwanttoeat50burger")
 
     elemPassword.send_keys(Keys.RETURN)
+    assert "Site administration" in driver.title
+    
 
 def test_comment_blank():
     driver = webdriver.Chrome()
@@ -48,15 +75,17 @@ def test_comment_blank():
     driver.find_element_by_link_text("Projects done").click()
 
     elemName = driver.find_element_by_name("author")
-    #elemName.send_keys("JJ")
+    elemName.send_keys("")
 
-    #elemComment = driver.find_element_by_name("body")
-    #elemComment.send_keys("Hi!")
+    elemComment = driver.find_element_by_name("body")
+    elemComment.send_keys("")
 
     #elem.clear()
     elemName.send_keys(Keys.RETURN)
+    assert driver.find_element_by_css_selector("input:invalid")
+    
 
-def test_comment_name_blank():
+def test_comment_name_only():
     driver = webdriver.Chrome()
     driver.get("http://localhost:8000/blog/")
     driver.find_element_by_link_text("Projects done").click()
@@ -64,11 +93,27 @@ def test_comment_name_blank():
     elemName = driver.find_element_by_name("author")
     elemName.send_keys("JJ")
 
-    #elemComment = driver.find_element_by_name("body")
-    #elemComment.send_keys("Hi!")
+    elemComment = driver.find_element_by_name("body")
+    elemComment.send_keys("")
 
     #elem.clear()
     elemName.send_keys(Keys.RETURN)
+    assert driver.find_element_by_css_selector("textarea:invalid")
+
+def test_comment_comment_only():
+    driver = webdriver.Chrome()
+    driver.get("http://localhost:8000/blog/")
+    driver.find_element_by_link_text("Projects done").click()
+
+    elemName = driver.find_element_by_name("author")
+    elemName.send_keys("")
+
+    elemComment = driver.find_element_by_name("body")
+    elemComment.send_keys("Hi!")
+
+    #elem.clear()
+    elemName.send_keys(Keys.RETURN)
+    assert driver.find_element_by_css_selector("input:invalid")
 
 def test_comment():
     driver = webdriver.Chrome()
@@ -83,3 +128,11 @@ def test_comment():
 
     #elem.clear()
     elemName.send_keys(Keys.RETURN)
+    
+
+def test_accessible():
+    driver = webdriver.Chrome()
+    driver.get("http://localhost:8000/blog/")
+    driver.find_element_by_link_text("Projects done").click()
+    assert "Projects done" in driver.title
+    
